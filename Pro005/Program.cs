08c554fbc94572beb7e892913ce79c49d6ab3988
Danil <danila_monster@mail,ru>
2022-08-02 23:54:21 +0300

    if

---
+++ Pro005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Маша")
+{
+    Console.WriteLine("Ура, это кукуруза");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
